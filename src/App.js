4b0c293975cbea0fd3d00a54f0fcc5a99534e018
Jethet <henriette.hettinga@gmail.com>
2020-03-25 16:47:57 +0100

    Create form, get API link and define result from axios.get call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// link to GH API: https://api.github.com/users/
+
+import UserForm from "./components/UserForm";
+
+class App extends Component {
+  state = {
+    repos: null
+  };
+
+  getUser = e => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    axios.get(`https://api.github.com/users/${user}`).then(res => {
+      const repos = res.data.public_repos;
+      this.setState({ repos: repos });
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">HTTP calls in React</h1>
+        </header>
+        <UserForm getUser={this.getUser} />
+      </div>
+    );
+  }
 }
 
 export default App;
