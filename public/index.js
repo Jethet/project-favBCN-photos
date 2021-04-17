0fc05c8d4d703a55b15e0a8ebd4cc6f991e94385
Jethet <henriette.hettinga@protonmail.com>
2021-04-17 15:32:30 +0200

    Add photos; add list of credits in footer

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
-
 let imagesArray = [
+  "https://1.bp.blogspot.com/-9OxXyVavxoI/T1TsRCO5OlI/AAAAAAAAHjc/qqNGhDmiCBw/s1600/ES_Bar_PanoramawSagrada.jpg",
   "https://www.barcelonaborne.com/wp-content/uploads/2019/02/Ajuntament_de_Barcelona_-e1493376692916-760x428.jpg",
   "https://cdn.thecrazytourist.com/wp-content/uploads/2016/07/Barcelona-City-Beach.jpg",
   "https://www.citylifebarcelona.com/wp-content/uploads/barcab.jpg",
@@ -9,8 +9,16 @@ let imagesArray = [
   "https://2.bp.blogspot.com/-lPfS-jj6nog/Tok3FT1Nb2I/AAAAAAAADn0/ODXxQkGxXY8/s1600/barcelona-city-2011-photo-4.jpg",
   "https://urbansurf.me/wp-content/uploads/2019/05/Barcelona-City-Attractions.jpg",
   "https://www.cooltur.org/wp-content/uploads/2018/10/COOLTUR-Turisme-Cultural-ENTRADA-ROMANA-CATEDRAL-BARCELONA.jpg",
+  "https://www.pride.com/sites/www.pride.com/files/2015/12/23/sagradafamilia.jpg",
+  "https://media.timeout.com/images/101851347/image.jpg",
+  "https://getwallpapers.com/wallpaper/full/6/7/b/1087965-full-size-barcelona-city-wallpapers-2941x1961-for-hd-1080p.jpg",
+  "https://wallpapercave.com/wp/wp1825724.jpg",
+  "https://getwallpapers.com/wallpaper/full/a/d/e/1088400-barcelona-city-wallpapers-1920x1080-ipad-retina.jpg",
+  "https://wallpapercave.com/wp/wp1825725.jpg",
+  "https://getwallpapers.com/wallpaper/full/1/5/e/1088009-free-download-barcelona-city-wallpapers-1920x1080-retina.jpg",
+  "http://blog.via.com/wp-content/uploads/2016/06/barcelona.jpg",
+  "https://www.barcelona.com/var/plain/storage/images/barcelona_hotels/hotel_barcelona_city_centre/header_container_city_center/header_images_centre_2/9248722-2-eng-GB/header_images_centre_2_header.jpg"
 ];
-
 
 //  https://api.unsplash.com/
 
